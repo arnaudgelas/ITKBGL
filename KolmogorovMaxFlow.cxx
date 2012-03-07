50d0a6115afa8cd3e686c7f523886e33fedebb1d
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-03-07 10:37:00 -0500

    Add MinCut

---
+++ KolmogorovMaxFlow.cxx
@@ -0,0 +1,85 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageBoostGraphAdaptor.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+
+#include <boost/graph/boykov_kolmogorov_max_flow.hpp>
+
+int main( int argc, char* argv[] )
+  {
+  if( argc != 2 )
+    {
+    std::cerr << argv[0] << " <InputImage>" << std::endl;
+    return EXIT_FAILURE;
+    }
+  typedef unsigned char PixelType;
+  const unsigned int Dimension = 2;
+
+  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  reader->Update();
+
+  ImageType::Pointer input = reader->GetOutput();
+
+  typedef itk::ImageBoostGraphAdaptor< ImageType > AdaptorType;
+
+  std::vector< AdaptorType::NeighborhoodIteratorOffsetType > offset( 8 );
+
+  size_t k = 0;
+  offset[k][0] = -1;
+  offset[k][1] = -1;
+  k++;
+
+  offset[k][0] = -1;
+  offset[k][1] = 0;
+  k++;
+
+  offset[k][0] = -1;
+  offset[k][1] = 1;
+  k++;
+
+  offset[k][0] = 0;
+  offset[k][1] = -1;
+  k++;
+
+  offset[k][0] = 0;
+  offset[k][1] = 1;
+  k++;
+
+  offset[k][0] = 1;
+  offset[k][1] = -1;
+  k++;
+
+  offset[k][0] = 1;
+  offset[k][1] = 0;
+  k++;
+
+  offset[k][0] = 1;
+  offset[k][1] = 1;
+  k++;
+
+
+  AdaptorType::Pointer adaptor = AdaptorType::New();
+  adaptor->SetInput( input );
+  adaptor->SetNeighbors( offset );
+  adaptor->Update();
+
+  std::cout << "Graph constructed" << std::endl;
+
+  typedef AdaptorType::GraphType GraphType;
+  GraphType graph = adaptor->GetOutput();
+
+  typedef AdaptorType::VertexDescriptorType   VertexDescriptorType;
+  typedef AdaptorType::EdgeDescriptorType     EdgeDescriptorType;
+  typedef AdaptorType::WeightType             WeightType;
+
+  VertexDescriptorType s, t;
+
+  flow = boykov_kolmogorov_max_flow(graph, s, t); // a list of sources will be returned in s, and a list of sinks will be returned in t
+
+  return EXIT_SUCCESS;
+  }
+
---
+++ MinCut.cxx
@@ -0,0 +1,93 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageBoostGraphAdaptor.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+
+#include <boost/graph/one_bit_color_map.hpp>
+#include <boost/graph/stoer_wagner_min_cut.hpp>
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 2 )
+    {
+    std::cerr << argv[0] << " <InputImage>" << std::endl;
+    return EXIT_FAILURE;
+    }
+  typedef unsigned char PixelType;
+  const unsigned int Dimension = 2;
+
+  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  reader->Update();
+
+  ImageType::Pointer input = reader->GetOutput();
+
+  typedef itk::ImageBoostGraphAdaptor< ImageType > AdaptorType;
+
+  std::vector< AdaptorType::NeighborhoodIteratorOffsetType > offset( 4 );
+
+  size_t k = 0;
+  offset[k][0] = -1;
+  offset[k][1] = 0;
+  k++;
+
+  offset[k][0] = 0;
+  offset[k][1] = -1;
+  k++;
+
+  offset[k][0] = 0;
+  offset[k][1] = 1;
+  k++;
+
+  offset[k][0] = 1;
+  offset[k][1] = 0;
+  k++;
+
+  AdaptorType::Pointer adaptor = AdaptorType::New();
+  adaptor->SetInput( input );
+  adaptor->SetNeighbors( offset );
+  adaptor->Update();
+
+  std::cout << "Graph constructed" << std::endl;
+
+  typedef AdaptorType::GraphType GraphType;
+  GraphType graph = adaptor->GetOutput();
+
+  typedef AdaptorType::VertexDescriptorType   VertexDescriptorType;
+  typedef AdaptorType::WeightType             WeightType;
+
+  // define a property map, `parities`, that will store a boolean value for each vertex.
+  // Vertices that have the same parity after `stoer_wagner_min_cut` runs are on the same side of the min-cut.
+  BOOST_AUTO( parities, boost::make_one_bit_color_map( num_vertices( graph ), get( boost::vertex_index, graph ) ) );
+
+  // run the Stoer-Wagner algorithm to obtain the min-cut weight. `parities` is also filled in.
+  int w = boost::stoer_wagner_min_cut( graph, get( boost::edge_weight, graph ), boost::parity_map( parities ) );
+
+  std::cout << "The min-cut weight of G is " << w << ".\n" << std::endl;
+
+  /*
+  std::cout << "One set of vertices consists of:" << std::endl;
+  size_t i;
+  for( i = 0; i < num_vertices(g); ++i)
+    {
+    if( get(parities, i) )
+      {
+      std::cout << i << std::endl;
+      }
+    }
+
+  std::cout << "The other set of vertices consists of:" << endl;
+  for (i = 0; i < num_vertices(g); ++i)
+    {
+    if (!get(parities, i))
+      {
+      std::cout << i << std::endl;
+      }
+    }
+  std::cout << std::endl;*/
+
+  return EXIT_SUCCESS;
+}
